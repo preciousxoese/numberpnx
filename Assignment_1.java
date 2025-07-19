53da87fc90a6df4b8731f0ef533df1ee45113c38
Xoese <preciousxoese40@gmail.com>
2025-07-19 22:18:17 +0000

    used the try command

--- Assignment_1.java
+++ Assignment_1.java
@@ -7,20 +7,19 @@ public class Assignment_1{
      * DESCRIPTION:POSITIVE,NEGATIVE AND ZERO
      */
     public static void main(String[] args){
-        Scanner sc = new Scanner (System.in);
-        int number;
-        System.out.print("Enter any number: ");
-        number = sc.nextInt();
-        if (number > 0){
-            System.out.println("The number is positive.");  
-        } 
-        else if (number < 0){
-            System.out.println("The number is negative.");
+        try (Scanner sc = new Scanner (System.in)) {
+            int number;
+            System.out.print("Enter any number: ");
+            number = sc.nextInt();
+            if (number > 0){
+                System.out.println("The number is positive.");
+            }
+            else if (number < 0){
+                System.out.println("The number is negative.");
+            }
+            else {
+                System.out.println("The number is zero.");
+            }
         }
-        else {
-            System.out.println("The number is zero.");
-        }
-
-    sc.close();
     }
 }
